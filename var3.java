0c2cc9b0d658333e25a0504daff4cef51bb2c4f0
WOOJUN NAM <102453742+wowai717@users.noreply.github.com>
2024-09-19 23:18:51 +0900

    java
    자바 강의 연습

---
+++ var3.java
@@ -0,0 +1,11 @@
+package javapritce;
+
+public class var3 {
+    public static void main(String[] args) {
+        int a; //변수 선언
+        a = 20; // 변수 초기화 a(20)
+        System.out.println(a);
+        a = 50; // 변수 값 변경 a(20 -> 50)
+        System.out.println(a);
+    }
+}
